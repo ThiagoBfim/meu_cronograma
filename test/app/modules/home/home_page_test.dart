3fba6a2382f0c91331b73ab6504c55987ecaa9f7
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-11-06 22:03:37 +0000

    Resolve "Incluir CI - Incluir no pipeline execução da analise do código e dos testes"

--- test/app/modules/home/home_page_test.dart
+++ test/app/modules/home/home_page_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
-
+import 'package:flutter_test/flutter_test.dart';
+import 'package:meu_cronograma/app/modules/home/home_module.dart';
 import 'package:meu_cronograma/app/modules/home/home_page.dart';
 
 void main() {
+  initModule(HomeModule());
   testWidgets('HomePage has title', (tester) async {
      await tester.pumpWidget(buildTestableWidget(HomePage(title: 'Home')));
      final titleFinder = find.text('Home');
